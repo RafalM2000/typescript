25a7865dcfc69fa6fbce17233f9fe1261dd15d46
RafalM2000 <rfm@europe.com>
2021-03-05 17:34:58 +0100

    parametry opcjonalne i domyślne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typescriptShortTutorial';
+export class AppComponent implements OnInit {
+  title: any = 'TYPESCRIPT';
+
+  ngOnInit(): void {
+    this.ulissesHeros("Buck Mulligan");
+  }
+
+  ulissesHeros(name: string = "Stefan Dedalus", sex?: string): void {
+    if (!sex) {
+      sex = 'Man';
+}
+
+    console.log(name, sex);
+  }
 }
